bcd23e6e4f90ea9fa8ec104cd129252585a1f12a
Sam Tchen <samtchen@gmail.com>
2015-08-27 16:18:31 -0700

    replace

---
+++ js/snake-view.js
@@ -0,0 +1,169 @@
+(function() {
+  if (typeof Game === "undefined") {
+    window.Game = {};
+  }
+
+  var View = Game.View = function ($el) {
+    this.$el = $el;
+    this.paused = false;
+    this.board = new Game.Board();
+    this.setupGrid();
+    this.setupStart();
+  };
+
+  View.prototype.setupStart = function () {
+    var start = "<button class=start>Start!</button>";
+    $('.button').html(start);
+    $('.start').one('click',this.start.bind(this));
+    $('.prompt').html("Press Start!")
+    this.prompt = setInterval(function () {
+      $('.prompt').toggleClass("white-font");
+    }, 700);
+  };
+
+  View.prototype.pause = function () {
+    this.paused = true;
+    $('.scores').addClass("white-font");
+    $('.overlay').removeClass("hidden");
+    $('.button').addClass("hidden");
+    $('.prompt').html("Press Space To Unpause")
+    clearInterval(this.interval);
+    this.prompt = setInterval(function () {
+      $('.prompt').toggleClass("white-font");
+    }, 700);
+  }
+
+  View.prototype.pauseHandler = function (event) {
+    if (event.keyCode == 32) {
+      if (this.paused) {
+        this.start();
+      } else {
+        this.pause();
+      }
+    }
+  }
+
+  View.prototype.start = function () {
+    $('body').off();
+    this.paused = false;
+    clearInterval(this.prompt);
+    $('.button').removeClass("hidden");
+    $('.prompt').removeClass("white-font");
+    $('.scores').removeClass("white-font");
+    $('.overlay').addClass("hidden");
+    this.bindEvent();
+    this.bindPause();
+    setTimeout(this.startInterval.bind(this),300);
+  };
+
+  View.prototype.bindPause = function () {
+    // this.boundPauseHandler = this.pauseHandler.bind(event,this);
+    // $('body').on('keydown', this.pauseHandler.bind(event));
+    $('body').on('keydown', function (event) {
+      if (event.keyCode == 32) {
+        if (this.paused) {
+          this.start();
+        } else {
+          this.pause();
+        }
+      }
+    }.bind(this));
+  };
+
+  View.prototype.startInterval = function () {
+    this.interval = setInterval(this.step.bind(this), 100);
+  };
+
+  View.prototype.setupRestart = function () {
+    $('.scores').addClass("white-font");
+    $('.overlay').removeClass("hidden");
+    $('.prompt').html("Game Over!")
+    this.prompt = setInterval(function () {
+      $('.prompt').toggleClass("white-font");
+    }, 700);
+    var restart = "<button class=restart>Restart!</button>";
+    $('.button').html(restart);
+    $('.restart').on('click', this.restart.bind(this));
+  };
+
+  View.prototype.restart = function () {
+    $('.overlay').removeClass("hidden");
+    clearInterval(this.prompt);
+    $('.scores').removeClass("white-font");
+    $('.prompt').removeClass("white-font");
+    $('.overlay').addClass("hidden");
+    this.board.snake.reset();
+    this.board.lose = false;
+    this.start();
+  };
+
+  View.prototype.render = function () {
+    // this.$el.empty();
+    // this.$el.text(this.board.render());
+    // if(this.board.snake.eatApple()){
+    //   debugger;
+    // }
+    this.updateClasses([this.board.apple.position], "apple");
+    this.updateClasses(this.board.snake.segments, "snake");
+  };
+
+  View.prototype.updateClasses = function(coords, className) {
+    this.$li.filter("." + className).removeClass();
+
+    coords.forEach(function(coord){
+      var flatCoord = (coord[0] * this.board.grid.length) + coord[1];
+      this.$li.eq(flatCoord).addClass(className);
+    }.bind(this));
+  };
+
+  View.prototype.setupGrid = function () {
+    var html = "";
+    for (var i = 0; i < this.board.grid.length; i++) {
+      html += "<ul data-col=" + i + ">";
+      for (var j = 0; j < this.board.grid.length; j++) {
+        html += "<li data-col=" + i + " data-row=" + j + "></li>";
+      }
+      html += "</ul>";
+    }
+
+    this.$el.html(html);
+    this.$li = this.$el.find("li");
+  }
+
+
+
+  View.prototype.bindEvent = function() {
+    var that = this;
+    $('body').on('keydown',function (event) {
+      if (event.keyCode === 87) {
+        that.board.snake.turn("N");
+        $('body').off('keydown', arguments.callee);
+      } else if (event.keyCode === 65) {
+        that.board.snake.turn("W");
+        $('body').off('keydown', arguments.callee);
+      } else if (event.keyCode === 83) {
+        that.board.snake.turn("S");
+        $('body').off('keydown', arguments.callee);
+      } else if (event.keyCode === 68) {
+        that.board.snake.turn("E");
+        $('body').off('keydown', arguments.callee);
+      }
+    });
+  };
+
+  View.prototype.step = function () {
+    this.bindEvent();
+    if (!this.board.lose) {
+      this.board.snake.move();
+      $('.score').html(this.board.snake.score);
+      $('.high-score').html(this.board.snake.highScore);
+      this.render();
+    } else {
+      $('body').off();
+      clearInterval(this.interval);
+      this.setupRestart();
+    }
+  };
+
+
+})();
---
+++ js/snake.js
@@ -0,0 +1,194 @@
+(function () {
+  if (typeof Game === "undefined") {
+    window.Game = {};
+  }
+
+  var Coord = Game.Coord = function () {}
+
+  Coord.VECTORS = {
+    "N": [-1, 0],
+    "E": [ 0, 1],
+    "S": [ 1, 0],
+    "W": [ 0,-1]
+  },
+
+  Coord.plus = function (coord, dir) {
+    return [
+      coord[0] + Coord.VECTORS[dir][0],
+      coord[1] + Coord.VECTORS[dir][1]
+    ];
+  },
+
+  Coord.equals = function (coord1, coord2) {
+    return (coord1[0] === coord2[0] && coord1[1] === coord2[1]);
+  },
+
+  Coord.isOpposite = function (curDir, newDir) {
+    return(
+        (curDir === "N" && newDir === "S") ||
+        (curDir === "S" && newDir === "N") ||
+        (curDir === "E" && newDir === "W") ||
+        (curDir === "W" && newDir === "E"));
+  };
+
+  var Snake = Game.Snake = function (board) {
+    this.dir = "S";
+    this.board = board;
+    var middle = Math.floor(this.board.grid.length / 2);
+    var startSegs = this.startingSegments([middle,middle]);
+    this.segments = startSegs;
+    this.head = this.segments[0];
+    this.growTurns = 0;
+    this.score = 0;
+    this.highScore = 0;
+  };
+
+  Snake.prototype.reset = function () {
+    this.dir = "S";
+    var middle = Math.floor(this.board.grid.length / 2);
+    var startSegs = this.startingSegments([middle,middle]);
+    this.segments = startSegs;
+    this.head = this.segments[0];
+    this.growTurns = 0;
+    this.score = 0;
+    this.board.apple.replaceApple();
+  }
+
+  Snake.prototype.includes = function (pos) {
+    var found = false;
+    this.segments.forEach( function (snakeSeg) {
+      if (Coord.equals(pos,snakeSeg)) {
+        found = true;
+      }
+    });
+    return found;
+  };
+
+  Snake.prototype.collision = function (pos) {
+    var found = false;
+    this.segments.slice(1,this.segments.length).forEach( function (snakeSeg) {
+      if (Coord.equals(pos,snakeSeg)) {
+        found = true;
+      }
+    }.bind(this));
+    return found;
+  };
+
+
+  Snake.prototype.startingSegments = function (pos) {
+    var nextPos = pos;
+    var segments = [];
+    for (var i = 0; i < 5; i++) {
+      nextPos = Coord.plus(nextPos, "N");
+      segments.push(nextPos);
+    }
+    return segments;
+  };
+
+  Snake.prototype.move = function () {
+    var nextPos = Coord.plus(this.head, this.dir);
+    if (this.eatApple()) {
+      this.score += 10;
+      if (this.highScore <= this.score) {
+        this.highScore = this.score;
+      }
+      this.board.apple.replaceApple();
+    }
+
+    if (this.validMove(nextPos)) {
+      this.segments.unshift(nextPos);
+      this.head = this.segments[0];
+
+
+      if (this.growTurns > 0) {
+        this.growTurns -= 1;
+      } else {
+        this.segments.pop();
+      }
+    } else {
+      this.board.lose = true;
+    }
+
+  };
+
+  Snake.prototype.validMove = function (pos) {
+    if (pos[0] >= this.board.grid.length ||
+        pos[1] >= this.board.grid.length ||
+        pos[0] < 0 ||
+        pos[1] < 0)
+    {
+      return false;
+    } else if (this.collision(pos)) {
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  Snake.prototype.turn = function (newDir) {
+    if (!Game.Coord.isOpposite(this.dir,newDir)) {
+      this.dir = newDir;
+    }
+  };
+
+  Snake.prototype.eatApple = function () {
+    if (Coord.equals(this.board.apple.position, Coord.plus(this.head, this.dir))) {
+      this.growTurns += 3;
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  var Board = Game.Board = function () {
+    this.grid = this.newGrid();
+    this.snake = new Snake(this);
+    this.apple = new Apple(this);
+    this.apple.replaceApple();
+    this.lose = false;
+  };
+
+  Board.prototype.newGrid = function() {
+    var grid = new Array(20);
+    for (var i = 0; i < grid.length; i++) {
+      grid[i] = new Array(20);
+    }
+    return grid;
+  };
+
+  Board.prototype.render = function () {
+    var string = "";
+    for (var i = 0; i < this.grid.length; i++) {
+      for (var j = 0; j < this.grid[i].length; j++) {
+        string += " ";
+        if (this.snake.includes([i,j])) {
+          string += " s ";
+        } else if (Coord.equals(this.apple.position,[i,j])) {
+          string += " a ";
+        } else {
+          string += " . ";
+        }
+        string += " ";
+      }
+      string += "\n";
+    }
+    return string;
+  };
+
+  var Apple = Game.Apple = function (board) {
+    this.board = board;
+    this.replaceApple();
+  };
+
+  Apple.prototype.replaceApple = function () {
+    var xCoord = Math.floor(Math.random() * this.board.grid.length);
+    var yCoord = Math.floor(Math.random() * this.board.grid.length);
+    while (this.board.snake.includes([xCoord,yCoord])) {
+      xCoord = Math.floor(Math.random() * this.board.grid.length);
+      yCoord = Math.floor(Math.random() * this.board.grid.length);
+    };
+
+    this.position = [xCoord,yCoord];
+  };
+
+})();
